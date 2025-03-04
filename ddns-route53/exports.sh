7cdea1bed242fde6c1f9637e6acb52f4fa4b55c2
dancpluz <dan08jan@gmail.com>
2025-03-04 13:55:28 +0000

    Initial commit: DDNS Route53 app

---
+++ ddns-route53/exports.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# User must replace these values with their own
+export TZ="America/Sao_Paulo"
+export SCHEDULE="*/10 * * * *"
+export LOG_LEVEL="info"
+export LOG_JSON="true"
+export AWS_ACCESS_KEY_ID="REPLACE_WITH_YOUR_AWS_ACCESS_KEY"
+export AWS_SECRET_ACCESS_KEY="REPLACE_WITH_YOUR_AWS_SECRET_KEY"
+export HOSTED_ZONE_ID="REPLACE_WITH_YOUR_HOSTED_ZONE_ID"
+export DOMAIN_NAME="REPLACE_WITH_YOUR_DOMAIN"
